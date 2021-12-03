ef79f7316facab927fc3c4e05b421f5ab204ea50
Johan Walles <johan.walles@gmail.com>
2021-12-03 19:08:59 +0100

    Introduce LALRPOP parser
    FIXME:
    * Convert grammar to parse command lines, not numbers from the LALRPOP
      tutorial
    * Replace our manually written string splitting parser with the LALRPOP
      parser

---
+++ build.rs
@@ -0,0 +1,7 @@
+// Ref: http://lalrpop.github.io/lalrpop/tutorial/001_adding_lalrpop.html
+
+extern crate lalrpop;
+
+fn main() {
+    lalrpop::process_root().unwrap();
+}
--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,11 @@
-#![allow(clippy::needless_return)]
+#[macro_use]
+extern crate lalrpop_util;
+
+lalrpop_mod!(pub commandline); // synthesized by LALRPOP
 
 mod parser;
 
 fn main() {
+    commandline::TermParser::new().parse("22").is_ok();
     println!("Hello World!");
 }
