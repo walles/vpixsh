113e7a5215b00fbd68c05f94dbc4f04a360f20d8
Johan Walles <johan.walles@gmail.com>
2022-01-01 10:01:23 +0100

    Move cd implementation into its own file

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,11 @@
 #![allow(clippy::needless_return)]
 
+use std::env;
 use std::io;
 use std::io::Write;
 use std::os::unix::prelude::ExitStatusExt;
-use std::path::{Path, PathBuf};
+use std::path::PathBuf;
 use std::process::Command;
-use std::{env, fs};
 
 use rustyline::error::ReadlineError;
 
@@ -13,6 +13,7 @@ use crate::ansicolor::{green, red};
 use crate::parser::{parse, Executor};
 
 mod ansicolor;
+mod cd;
 mod parser;
 mod tokenizer;
 
@@ -96,72 +97,6 @@ impl Shell {
                 }
             }
         }
-    }
-
-    fn cd(&mut self, args: &[String]) -> String {
-        if args.is_empty() {
-            let env_home = env::var("HOME");
-            if let Err(error) = env_home {
-                println!("ERROR: Cannot read HOME environment variable: {}", error);
-                return "HOME not set".to_string();
-            }
-            self.cd(&[env_home.unwrap()]);
-            return "".to_string();
-        }
-
-        if args.len() != 1 {
-            println!("ERROR: cd wanted zero or one argument, got {}", args.len());
-            return "Too many args".to_string();
-        }
-
-        let target = &args[0];
-
-        if target == "-" {
-            let temp = self.current_dir.to_owned();
-            self.current_dir = self.oldpwd.to_owned();
-            self.oldpwd = temp;
-            return "".to_string();
-        }
-
-        let mut target_path = PathBuf::from(&self.current_dir);
-
-        // Pushing resolves absolute paths
-        target_path.push(Path::new(target));
-
-        if !target_path.is_dir() {
-            println!("ERROR: Not a directory: {}", target);
-            return "Not a dir".to_string();
-        }
-
-        let canonicalize_result = target_path.canonicalize();
-        if let Err(error) = canonicalize_result {
-            println!(
-                "ERROR: Unable to canonicalize <{}>: {}",
-                target_path.to_string_lossy(),
-                error
-            );
-            return error.to_string();
-        }
-        target_path = canonicalize_result.unwrap();
-
-        if let Err(error) = fs::read_dir(target_path.to_owned()) {
-            println!(
-                "ERROR: Target directory <{}> is inaccessible: {}",
-                target_path.to_string_lossy(),
-                error
-            );
-            if let Some(os_error) = error.raw_os_error() {
-                if os_error == 13 {
-                    // "13" == EPERM
-                    return "Permission denied".to_string();
-                }
-            }
-            return error.to_string();
-        }
-
-        self.oldpwd = self.current_dir.to_owned();
-        self.current_dir = target_path;
-        return "".to_string();
     }
 
     /// Returns an error message for the prompt, or "" on success
