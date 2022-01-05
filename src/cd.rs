dbbb30bddac84d205b6fc14cad5ccf8943647be9
Johan Walles <johan.walles@gmail.com>
2022-01-05 10:20:43 +0100

    Split cd into more methods

--- src/cd.rs
+++ src/cd.rs
@@ -1,61 +1,40 @@
-use std::{
-    env, fs,
-    path::{Path, PathBuf},
-};
+use std::{env, fs, path::PathBuf};
 
 use crate::Shell;
 
 impl Shell {
-    pub(crate) fn cd(&mut self, args: &[String]) -> String {
-        if args.is_empty() {
-            let env_home = env::var("HOME");
-            if let Err(error) = env_home {
-                println!("ERROR: Cannot read HOME environment variable: {}", error);
-                return "HOME not set".to_string();
-            }
-            self.cd(&[env_home.unwrap()]);
-            return "".to_string();
+    fn cd_minus(&mut self) -> String {
+        self.current_dir = self.oldpwd.to_owned();
+        return "".to_string();
+    }
+
+    fn cd_directory(&mut self, mut target: PathBuf) -> String {
+        if target.is_relative() {
+            let mut absolute_target = PathBuf::from(&self.current_dir);
+            absolute_target.push(target);
+            target = absolute_target;
         }
 
-        if args.len() != 1 {
-            println!("ERROR: cd wanted zero or one argument, got {}", args.len());
-            return "Too many args".to_string();
-        }
-
-        let target = &args[0];
-
-        if target == "-" {
-            let temp = self.current_dir.to_owned();
-            self.current_dir = self.oldpwd.to_owned();
-            self.oldpwd = temp;
-            return "".to_string();
-        }
-
-        let mut target_path = PathBuf::from(&self.current_dir);
-
-        // Pushing resolves absolute paths
-        target_path.push(Path::new(target));
-
-        if !target_path.is_dir() {
-            println!("ERROR: Not a directory: {}", target);
+        if !target.is_dir() {
+            println!("ERROR: Not a directory: {}", target.to_string_lossy());
             return "Not a dir".to_string();
         }
 
-        let canonicalize_result = target_path.canonicalize();
+        let canonicalize_result = target.canonicalize();
         if let Err(error) = canonicalize_result {
             println!(
                 "ERROR: Unable to canonicalize <{}>: {}",
-                target_path.to_string_lossy(),
+                target.to_string_lossy(),
                 error
             );
             return error.to_string();
         }
-        target_path = canonicalize_result.unwrap();
+        target = canonicalize_result.unwrap();
 
-        if let Err(error) = fs::read_dir(target_path.to_owned()) {
+        if let Err(error) = fs::read_dir(target.to_owned()) {
             println!(
                 "ERROR: Target directory <{}> is inaccessible: {}",
-                target_path.to_string_lossy(),
+                target.to_string_lossy(),
                 error
             );
             if let Some(os_error) = error.raw_os_error() {
@@ -67,8 +46,38 @@ impl Shell {
             return error.to_string();
         }
 
-        self.oldpwd = self.current_dir.to_owned();
-        self.current_dir = target_path;
+        self.current_dir = target;
         return "".to_string();
+    }
+
+    pub(crate) fn cd(&mut self, args: &[String]) -> String {
+        if args.is_empty() {
+            let env_home = env::var("HOME");
+            if let Err(error) = env_home {
+                println!("ERROR: Cannot read HOME environment variable: {}", error);
+                return "HOME not set".to_string();
+            }
+            return self.cd(&[env_home.unwrap()]);
+        }
+
+        if args.len() != 1 {
+            println!("ERROR: cd wanted zero or one argument, got {}", args.len());
+            return "Too many args".to_string();
+        }
+
+        let dir_before = self.current_dir.clone();
+
+        let target = &args[0];
+        let problem: String;
+        if target == "-" {
+            problem = self.cd_minus();
+        } else {
+            problem = self.cd_directory(PathBuf::from(target));
+        }
+
+        if problem.is_empty() {
+            self.oldpwd = dir_before;
+        }
+        return problem;
     }
 }
